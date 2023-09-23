b2940f2658653d81447a5c0572777e8e98a9d432
Adarsh srivastav <adarshsrivastav375@gmail.com>
2023-09-23 16:38:50 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,40 @@
+import express, { request, response } from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+//import { Book } from "./models/bookmodel.js";
+import routes from "./routes/routes.js";
+import cors from "cors";
+
+const app = express();
+
+//middleware for parsing request body
+app.use(express.json());
+
+// middleware for handling corse policy
+// option 1 : allow all origin with default cors(*)
+app.use(cors());
+//option 2 custom origin
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content - Type"],
+//   })
+// );
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("welcome to the tutorial");
+});
+app.use("/books", routes);
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("app connected to database");
+    app.listen(PORT, () => {
+      console.log("app is listing to port" + PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
